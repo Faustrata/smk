e4bed1e8b9df2c2bd95f0cf8c85bb1853ef9f367
faustrata <akhmadfauzan114>
2016-11-28 08:22:06 +0700

    Perbaikan Tampilan Index dg Bootstrap

--- index.php
+++ index.php
@@ -2,23 +2,34 @@
 <html>
 <head>
 	<title>Login Form</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css" >
 </head>
 <body>
-<div class="col-md-6">
-    <form role="form" action="login_proses.php" method="post">
-        <div class="form-group">
-          <h4 style="color:grey;">username :</h4>
-            <input type="text" class="form-control" autocomplete="off" placeholder="Enter NIS"
-             name="username" required>
-        </div>
-        <div class="form-group">
-          <h4 style="color:grey">Password :</h4>
-             <input type="password" class="form-control" 
-             placeholder="Enter password" name="password" autocomplete="off" required>
-        </div>
-        <br>
-             <button type="submit" class="btn btn-warning btn-block" name="signin">Login</button>
-        </form>
+<div class="container">
+	<div class="row">
+		<div class="login-box">
+			<br><br><br><br><br><br>
+			<div class="col-md-4">
+			</div>
+			<div class="col-md-4">
+		    	<form class="form-vertical"  action="login_proses.php" method="post">
+			        <div class="form-group">
+			            <input type="text" class="form-control" autocomplete="off" placeholder="Enter NIS"
+			             name="username" required>
+			        </div>
+			        <div class="form-group">
+
+			             <input type="password" class="form-control" 
+			             placeholder="Enter password" name="password" autocomplete="off" required>
+			        </div>
+			        <br>
+		             <button type="submit" class="btn btn-warning btn-block" name="signin">Login</button>
+		        </form>
+		    </div>
+		</div>
     </div>
+ </div>
 </body>
 </html>
